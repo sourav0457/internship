f0ce62858dc99ea48bda4740381f7f51bd758dd2
Live session user <sandeep.k@betsol.com>
2016-10-23 10:19:08 +0000

    commit

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import models.ConnectionRequest;
 import models.Profile;
 import models.User;
 import play.mvc.Controller;
@@ -21,7 +22,7 @@ public class HomeController extends Controller {
     public Result getProfile(Long id){
 
         User user = User.find.byId(id);
-        Profile profile= user.profile.find.byId(id);
+        Profile profile = Profile.find.byId(user.profile.id);
 
         ObjectNode userJson = objectMapper.createObjectNode();
         userJson.put("emailid",user.email);
@@ -30,15 +31,40 @@ public class HomeController extends Controller {
         userJson.put("company",profile.company);
         userJson.set("connections", objectMapper.valueToTree(user.connections.stream().map(connection ->{
             ObjectNode connectionJson = objectMapper.createObjectNode();
-            Profile profile1= profile.find.byId(id);
+            Profile profile1= Profile.find.byId(id);
 
-            connectionJson.put("firstname",profile1.firstname);
-            connectionJson.put("lastname",profile1.lastname);
-            connectionJson.put("company",profile1.company);
-            connectionJson.put("id",profile1.id);
+            connectionJson.put("firstname", profile1.firstname);
+            connectionJson.put("lastname", profile1.lastname);
+            connectionJson.put("company", profile1.company);
+            connectionJson.put("id", connection.id);
             return connectionJson;
         }).collect(Collectors.toList())));
 
+        userJson.set("connectionRequests", objectMapper.valueToTree(user.connectionRequestReceived.stream().filter(
+                x -> x.status.equals(ConnectionRequest.Status.WAITING)).map(connectionRequest ->{
+            ObjectNode connectionRequestJson = objectMapper.createObjectNode();
+            User sender= User.find.byId(connectionRequest.sender.id);
+            Profile senderProfile= Profile.find.byId(sender.profile.id);
+            connectionRequestJson.put("id",connectionRequest.id);
+            connectionRequestJson.put("firstname",senderProfile.firstname);
+            return connectionRequestJson ;
+        }).collect(Collectors.toList())));
+
+        userJson.set("suggestions", objectMapper.valueToTree(User.find.all().stream()
+                .filter(x -> !user.equals(x))
+                .filter(x -> !user.connections.contains(x))
+                .filter(x -> !user.connectionRequestReceived.stream().map(y -> y.sender).collect(Collectors.toList()).contains(x))
+                .filter(x -> !user.connectionRequestSent.stream().map(y -> y.receiver).collect(Collectors.toList()).contains(x))
+                .map(x -> {
+                    ObjectNode suggestionJson = objectMapper.createObjectNode();
+                    Profile suggestionProfile = Profile.find.byId(x.profile.id);
+                    suggestionJson.put("id", x.id);
+                    suggestionJson.put("firstName", suggestionProfile.firstname);
+                    suggestionJson.put("lastName", suggestionProfile.lastname);
+                    return suggestionJson;
+                }).collect(Collectors.toList())));
+
+        return ok(userJson);
     }
-    }
+}
 
